49a55343cb0d2ba5202c748b12df60eda80dfba4
webshunternet <78706454+webshunter@users.noreply.github.com>
2021-06-26 10:44:49 +0700

    update change for codeigniter 3

--- Tabledx.php
+++ Tabledx.php
@@ -1,25 +1,22 @@
 <?php
-namespace App\Models;
-
-use CodeIgniter\Model;
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-
-class Tabledx extends Model{
+class Tabledx extends CI_Model{
     protected $ci_v = '4';
     protected $tb = null;
     protected $key = 'id';
     protected $rn = [];
-    public $create_time = 'tanggal_daftar';
-    public $update_time = 'tanggal_update';
-    public $delete_time = 'tanggal_hapus';
+    public $create_time = 'created_at';
+    public $update_time = 'updated_at';
+    public $delete_time = 'delete_set';
     protected $cp = [];
     protected $newcol = [];
     protected $builder = null;
     protected $condition = [];
-    protected $postget = [];
+    public $postget = [];
     protected $userid = null;
     protected $keys = "id";
     protected $allTable = [];
@@ -29,13 +26,9 @@ class Tabledx extends Model{
     protected $selectparams = " * ";
     public $debugsql = "";
     protected $unsave = false;
+    protected $debuger = false;
+    protected $lmt = "";
 
-    public function __construct(){
-        $this->session = \Config\Services::session();
-        $this->db = \Config\Database::connect();
-        helper(["auth"]);
-    }
-    
     public function user()
     {
         $this->userid = user()->id;
@@ -48,7 +41,6 @@ class Tabledx extends Model{
 
     public function table($var = NULL)
     {
-        $this->builder = $this->db->table($var);
         $this->tb = $var;
     }
 
@@ -56,7 +48,7 @@ class Tabledx extends Model{
     {
         $this->newcol[$name] = $var;
     }
-    
+
     public function setkey($var = NULL)
     {
         $this->key = $var;
@@ -65,12 +57,17 @@ class Tabledx extends Model{
     // get data
     public function getData($el = NULL)
     {
-        return $this->db->query("SELECT * FROM $this->tb ")->getResult();
+        return $this->db->query("SELECT * FROM $this->tb ")->result();
     }
-    
+
     public function get($el)
     {
-        return $this->db->query($el)->getResult();
+        return $this->db->query($el)->result();
+    }
+
+    public function limit($start = 0, $length = 10)
+    {
+      $this->lmt = " LIMIT  $start, $length ";
     }
 
     public function getResult()
@@ -80,23 +77,99 @@ class Tabledx extends Model{
             $cond = array_keys($this->condition);
 
             $condconf = " WHERE ";
-
             foreach($cond as $eml => $cop){
                 $dat = $this->condition[$cop];
-                if($eml == 0){
-                    $condconf .= " $cop = \"$dat\" ";
+
+                if (is_array($dat)) {
+                  if($eml == 0){
+                    $opsi = $dat['opsi'];
+                    $val = $dat['val'];
+                    $condconf .= " $cop $opsi \"$val\" ";
+                  }else{
+                    $opsi = $dat['opsi'];
+                    $val = $dat['val'];
+                    $condconf .= " AND $cop $opsi \"$val\" ";
+                  }
                 }else{
+                  if($eml == 0){
+                    $condconf .= " $cop = \"$dat\" ";
+                  }else{
                     $condconf .= " AND $cop = \"$dat\" ";
+                  }
                 }
             }
-            $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf AND $this->tb.$this->delete_time IS NULL ";
-            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf AND $this->tb.$this->delete_time IS NULL ")->getResult();
+            $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf AND $this->tb.$this->delete_time = '0' $this->lmt ";
+            if ($this->debuger == true) {
+              echo "<pre>";
+              echo $this->debugsql;
+              echo "</pre>";
+            }
+            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf AND $this->tb.$this->delete_time = '0' $this->lmt ")->result();
         }else{
-            $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time IS NULL ";
-            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time IS NULL ")->getResult();
+            $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time = '0' $this->lmt ";
+            if ($this->debuger == true) {
+              echo "<pre>";
+              echo $this->debugsql;
+              echo "</pre>";
+            }
+            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time = '0' $this->lmt ")->result();
         }
     }
-    
+
+
+    public function debug()
+    {
+      $this->debuger = true;
+    }
+
+    public function num_rows()
+    {
+
+      if(count($this->condition) > 0){
+          $cond = array_keys($this->condition);
+
+          $condconf = " WHERE ";
+          foreach($cond as $eml => $cop){
+              $dat = $this->condition[$cop];
+
+              if (is_array($dat)) {
+                if($eml == 0){
+                  $opsi = $dat['opsi'];
+                  $val = $dat['val'];
+                  $condconf .= " $cop $opsi \"$val\" ";
+                }else{
+                  $opsi = $dat['opsi'];
+                  $val = $dat['val'];
+                  $condconf .= " AND $cop $opsi \"$val\" ";
+                }
+              }else{
+                if($eml == 0){
+                  $condconf .= " $cop = \"$dat\" ";
+                }else{
+                  $condconf .= " AND $cop = \"$dat\" ";
+                }
+              }
+          }
+          $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf AND $this->tb.$this->delete_time = '0' ";
+
+          if ($this->debuger == true) {
+            echo "<pre>";
+            echo $this->debugsql;
+            echo "</pre>";
+          }
+
+          return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf AND $this->tb.$this->delete_time = '0' ")->num_rows();
+      }else{
+          $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time = '0' ";
+          if ($this->debuger == true) {
+            echo "<pre>";
+            echo $this->debugsql;
+            echo "</pre>";
+          }
+          return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time = '0' ")->num_rows();
+      }
+    }
+
     public function getResultAll()
     {
 
@@ -114,10 +187,10 @@ class Tabledx extends Model{
                 }
             }
             $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf";
-            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf")->getResult();
+            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf")->result();
         }else{
             $this->debugsql = "SELECT $this->selectparams FROM $this->tb $this->leftjoin";
-            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin")->getResult();
+            return $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin")->result();
         }
     }
 
@@ -135,14 +208,14 @@ class Tabledx extends Model{
     public function tablecreate($var = "")
     {
         /**
-         * 
+         *
          * $this rn = [
          *      'row' => 'index name'
          * ]
-         * 
-         * 
+         *
+         *
          * disable name width param string rn-no inn index name
-         * 
+         *
          */
         $name = array_keys($this->rn);
         $table = "<table $var >";
@@ -160,8 +233,8 @@ class Tabledx extends Model{
                 $table .= "<th>";
                 $table .= $mmq;
                 $table .= "</th>";
-            }            
-        }            
+            }
+        }
         $table .= "</tr>";
         $table .= "</thead>";
         $table .= "<tbody>";
@@ -191,7 +264,7 @@ class Tabledx extends Model{
                         $table .= $elm->$cc;
                         $table .= "</td>";
                     }
-                }           
+                }
             }
             if (count($this->newcol) > 0) {
                 foreach(array_keys($this->newcol) as $mmq){
@@ -213,7 +286,7 @@ class Tabledx extends Model{
         return $table;
     }
 
-    
+
     public function createOpsi( $val = NULL, $name = NULL, $opsionalSelected = NULL )
     {
         $table = "";
@@ -231,7 +304,7 @@ class Tabledx extends Model{
         }
         return $table;
     }
-    
+
 
     public function addTable($table = NULL, $params = NULL, $primarytable = NULL, $key = NULL)
     {
@@ -256,7 +329,7 @@ class Tabledx extends Model{
             }
         }
     }
-    
+
     public function key($key = "id")
     {
         $this->keys = $key;
@@ -279,12 +352,34 @@ class Tabledx extends Model{
         }
     }
 
-    public function getInput()
+    public function getInput($ff = null)
     {
         $data = $_POST;
-        unset($data["csrf_test_name"]);
+        if ($ff != NULL) {
+            $data = $ff;
+        }
+        if(isset($data["csrf_test_name"])){
+            unset($data["csrf_test_name"]);
+        }
+
+        $getKeys = array_keys($data);
+
+        foreach ($getKeys as $ev => $aw) {
+            $pantern = "/_nbr4/i";
+            if (preg_match($pantern, $aw)) {
+              unset($data[$aw]);
+            }
+        }
+
         $this->postget = $data;
         return $data;
+    }
+
+    public function dataInput()
+    {
+        echo "<pre>";
+        print_r($this->postget);
+        echo "</pre>";
     }
 
     public function filePost($data, $path, $id = "", $table = "")
@@ -297,7 +392,7 @@ class Tabledx extends Model{
 			$gambar = $this->row();
             $gambar = (object) $gambar;
 		}
-        
+
         $nms = $data;
 		$data = $_FILES[$data];
 		$ext = pathinfo($data['name'], PATHINFO_EXTENSION);
@@ -325,18 +420,21 @@ class Tabledx extends Model{
     {
         $data = NULL;
         if (count($this->postget) > 0) {
-            $data = $this->postget;(array)
+            $data = $this->postget;
         }
         if (count($var) > 0) {
             $data = $var;
         }
-        return $this->builder->insert($data);
+        return $this->db->insert($this->tb, $data);
     }
 
-
-    public function leftJoin($params = " * ")
+    public function select($params = ' * ')
     {
-        $this->selectparams = $params;
+      $this->selectparams = $params;
+    }
+
+    public function leftJoin()
+    {
         $this->leftjoin = "";
         foreach($this->allTable as $key => $val){
             $setone = NULL;
@@ -361,52 +459,79 @@ class Tabledx extends Model{
     }
 
 
-    public function row()
+    public function row($dataName = null, $err = null)
     {
 
         if(count($this->condition) > 0){
             $cond = array_keys($this->condition);
             $condconf = " WHERE ";
-            $o = 0;
             foreach($cond as $eml => $cop){
                 $dat = $this->condition[$cop];
-                if($o == 0){
-                    $condconf .= " $cop = \"$dat\" ";
+
+                if (is_array($dat)) {
+                  if($eml == 0){
+                    $opsi = $dat['opsi'];
+                    $val = $dat['val'];
+                    $condconf .= " $cop $opsi \"$val\" ";
+                  }else{
+                    $opsi = $dat['opsi'];
+                    $val = $dat['val'];
+                    $condconf .= " AND $cop $opsi \"$val\" ";
+                  }
                 }else{
+                  if($eml == 0){
+                    $condconf .= " $cop = \"$dat\" ";
+                  }else{
                     $condconf .= " AND $cop = \"$dat\" ";
+                  }
                 }
-                $o++;
             }
 
-            $savemoment = "AND $this->tb.$this->delete_time IS NULL";
+            $savemoment = "AND $this->tb.$this->delete_time = '0'";
 
             if ($this->unsave != false) {
                 $savemoment = "";
             }
 
-            $elm = $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf $savemoment")->getResult();
-            if (count($elm) > 0) {
-                return $elm[0];
+            $elm = $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $condconf $savemoment $this->lmt")->row();
+            if ($elm != null) {
+                if ($dataName != null) {
+                  if($elm->$dataName != NULL){
+                    return $elm->$dataName;
+                  }else{
+                    return $err;
+                  }
+                }else{
+                  return $elm;
+                }
             }else{
-                return null;
+                return $err;
             }
         }else{
-            $savemoment = "WHERE $this->tb.$this->delete_time IS NULL";
+            $savemoment = "WHERE $this->tb.$this->delete_time = '0'";
             if ($this->unsave != false) {
                 $savemoment = "";
             }
-            $elm = $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $savemoment")->getResult();
-            if (count($elm) > 0) {
-                return $elm[0];
+            $elm = $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin $savemoment")->row();
+            if ($elm != null) {
+              if ($dataName != null) {
+                if($elm->$dataName != NULL){
+                  return $elm->$dataName;
+                }else{
+                  return $err;
+                }
+              }else{
+                return $elm;
+              }
             }else{
-                return null;
+                return $err;
             }
         }
     }
 
     public function getLast()
     {
-        $elm = $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time IS NULL ORDER BY $this->key DESC ")->getResult();
+        $elm = $this->db->query("SELECT $this->selectparams FROM $this->tb $this->leftjoin WHERE $this->tb.$this->delete_time = '0' ORDER BY $this->key DESC ")->result();
         if (count($elm) > 0) {
             return $elm[0];
         }else{
@@ -438,14 +563,13 @@ class Tabledx extends Model{
         if (count($this->condition) > 0) {
             $condition = $this->condition;
         }
-
         foreach(array_keys($set) as $el){
-            $this->builder->set($el, $set[$el]);
+            $this->db->set($el, $set[$el]);
         }
         foreach(array_keys($condition) as $el){
-            $this->builder->where($el, $condition[$el]);
+            $this->db->where($el, $condition[$el]);
         }
-        return $this->builder->update();
+        return $this->db->update($this->tb);
     }
 
     public function softDelete()
@@ -470,11 +594,10 @@ class Tabledx extends Model{
         if (count($this->condition) != 0) {
             $condition = $this->condition;
         }
-
         foreach(array_keys($condition) as $el){
-            $this->builder->where($el, $condition[$el]);
+            $this->db->where($el, $condition[$el]);
         }
-        $this->builder->delete();
+        $this->db->delete($this->tb);
     }
 
 
@@ -496,8 +619,8 @@ class Tabledx extends Model{
         if (count($this->newcol) > 0) {
             foreach(array_keys($this->newcol) as $mmq){
                 $headers[] = $mmq;
-            }            
-        }  
+            }
+        }
 
         for ($i = 0, $l = sizeof($headers); $i < $l; $i++) {
             $sheet->setCellValueByColumnAndRow($i + 1, 1, $headers[$i]);
@@ -525,7 +648,7 @@ class Tabledx extends Model{
                     }else{
                         $kap[] = $elm->$cc;
                     }
-                }           
+                }
             }
             if (count($this->newcol) > 0) {
                 foreach(array_keys($this->newcol) as $mmq){
@@ -553,8 +676,6 @@ class Tabledx extends Model{
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment; filename="'. urlencode($fileName).'"');
         $writer->save('php://output');
+
     }
-
-
-
 }
